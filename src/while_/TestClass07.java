a96b7deff8b953b602875ded97e8e89fc949cf65
seohee <test@test.123>
2023-06-16 17:09:10 +0900

    문자열 비교(equals)

---
+++ src/while_/TestClass07.java
@@ -0,0 +1,24 @@
+package while_;
+
+import java.util.Scanner;
+
+public class TestClass07 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name1, name2;
+		System.out.print("1이름 입력 : ");
+		name1 = sc.next();
+		System.out.print("2이름 입력 : ");
+		name2 = sc.next();
+		
+		System.out.println("n1 == n2 : "+ (name1==name2));
+		System.out.println("n1.equals(n2) : "+(name1.equals(name2)));
+		
+		//if(name1 == name2)
+		if(name2.equals(name1)) {
+			System.out.println("이름이 같다");
+		}else {
+			System.out.println("다르다");
+		}
+	}
+}
